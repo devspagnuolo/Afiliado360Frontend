af5dd882502c04aafb3c72a4b18af21b27f39573
devspagnuolo <rafaelspagnuologarcia@gmail.com>
2025-03-30 19:45:12 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Products from './pages/Products';
+import Login from './pages/Login';
 
 function App() {
+  const isAuthenticated = !!localStorage.getItem('token');
+
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/products" />} />
-      <Route path="/products" element={<Products />} />
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/products"
+        element={isAuthenticated ? <Products /> : <Navigate to="/login" />}
+      />
     </Routes>
   );
 }
