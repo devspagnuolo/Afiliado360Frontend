c15032252748bc5108f29825531abb7b45c326d4
devspagnuolo <rafaelspagnuologarcia@gmail.com>
2025-04-01 15:28:12 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,17 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Products from './pages/Products';
 import NewProduct from './pages/NewProduct';
-import ProtectedRoute from './components/ProtectedRoute';
 import HotmartTracker from './pages/HotmartTracker';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
-      <Route
-        path="/products"
-        element={
-          <ProtectedRoute>
-            <Products />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/products/new"
-        element={
-          <ProtectedRoute>
-            <NewProduct />
-          </ProtectedRoute>
-
-          <Route
-  path="/rastreador"
-  element={
-    <ProtectedRoute>
-      <HotmartTracker />
-    </ProtectedRoute>
-  }
-/>
-      
-        }
-      />
+      <Route path="/products" element={<ProtectedRoute><Products /></ProtectedRoute>} />
+      <Route path="/products/new" element={<ProtectedRoute><NewProduct /></ProtectedRoute>} />
+      <Route path="/rastreador" element={<ProtectedRoute><HotmartTracker /></ProtectedRoute>} />
     </Routes>
   );
 }
